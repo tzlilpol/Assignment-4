3e93ce3e0cb38e1e6a0b949343690c208c262afc
tlolpop <tzlilpol@post.bgu.ac.il>
2020-06-23 22:34:05 +0300

    DataPreparation.py

--- GUI.py
+++ GUI.py
@@ -1,40 +1,52 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
+from tkinter.filedialog import askopenfilename
+
+import xlrd
+import numpy as np
+import pandas as pd
+from DataPreparation import preProcessData
+class Application:
+    def __init__(self, master):
+        self.master = master
+        master.title("Assignment4")
+
+        self.pathLabel = Label(master, text='File Path')
+        self.filePath = StringVar()
+
+        self.browseInput=Entry(master,width=50,borderwidth=1, textvariable=self.filePath)
+        self.browseInput.grid(row=1,column=0)
+
+        self.clustersInput=Entry(master,width=30,borderwidth=1)
+        self.clustersInput.grid(row=3,column=0)
+
+        self.runsInput=Entry(master,width=30,borderwidth=1)
+        self.runsInput.grid(row=5,column=0)
+
+        # Create a Label Widget
+        self.pathChooseLbl=Label(master,text="Please enter dataset path:").grid(row=0,column=0)
+        self.clustersLbl=Label(master,text="Number of clusters k:").grid(row=2,column=0)
+        self.runsLbl=Label(master,text="Number of runs:").grid(row=4,column=0)
+        #Create a buttons
+        self.browseBtn=Button(master,text="Browse", command=lambda: self.openFile()).grid(row=1,column=1)
+        self.preProcessBtn=Button(master,text="Pre-process", command=lambda:self.preProcessData()).grid(row=6,column=0)
+        self.clusterBtn=Button(master,text="Cluster", command=lambda:self.buildModal()).grid(row=6,column=1)
+        self.dataSet = None
+        self.DataFrame = None
+
+         # Functions
+    def openFile(self):
+        self.filePath.set(askopenfilename())
+
+    def preProcessData(self):
+        self.dataSet=pd.read_excel(self.filePath.get())
+        print(self.dataSet)
+        self.DataFrame = preProcessData(self.dataSet)
+        messagebox.showinfo(title='K Means Clustering', message="Preprocessing completed successfully!")
+
+    def buildModal(self):
+        lbl = Label(self.master, text="Build Modal...").grid(row=7, column=1)
 
 root = Tk()
-root.title("Assignment4")
-browseInput=Entry(root,width=50,borderwidth=1)
-browseInput.grid(row=1,column=0)
-
-clustersInput=Entry(root,width=30,borderwidth=1)
-clustersInput.grid(row=3,column=0)
-
-runsInput=Entry(root,width=30,borderwidth=1)
-runsInput.grid(row=5,column=0)
-# e.delete(0,"")
-path=""
-#Functions
-def openFile():
-    path=filedialog.askopenfilename()
-    browseInput.insert(0,path)
-def preProcessData():
-    lbl=Label(root,text="Data ready...").grid(row=7,column=0)
-def buildModal():
-    lbl=Label(root,text="Build Modal...").grid(row=7,column=1)
-
-# Create a Label Widget
-pathChooseLbl=Label(root,text="Please enter dataset path:")
-clustersLbl=Label(root,text="Number of clusters k:")
-runsLbl=Label(root,text="Number of runs:")
-#Create a buttons
-browseBtn=Button(root,text="Browse",command=openFile)
-preProcessBtn=Button(root,text="Pre-process",command=preProcessData)
-clusterBtn=Button(root,text="Cluster",command=buildModal)
-# Shoving it onto screen
-pathChooseLbl.grid(row=0,column=0)
-clustersLbl.grid(row=2,column=0)
-runsLbl.grid(row=4,column=0)
-browseBtn.grid(row=1,column=1)
-preProcessBtn.grid(row=6,column=0)
-clusterBtn.grid(row=6,column=1)
+my_gui=Application(root)
 root.mainloop()
